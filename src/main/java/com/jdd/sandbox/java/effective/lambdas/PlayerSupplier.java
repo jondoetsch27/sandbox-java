cccfeadd98886449d8ca8b5113392a402560619d
jondoetsch27 <jonathon.doetsch@gmail.com>
2021-07-20 10:17:46 -0700

    07/20 - removed raw Supplier reference @jondoetsch27

--- src/main/java/com/jdd/sandbox/java/effective/lambdas/PlayerSupplier.java
+++ src/main/java/com/jdd/sandbox/java/effective/lambdas/PlayerSupplier.java
@@ -3,7 +3,7 @@ package com.jdd.sandbox.java.effective.lambdas;
 import java.util.function.Supplier;
 
 @FunctionalInterface
-public interface PlayerSupplier extends Supplier {
+public interface PlayerSupplier extends Supplier<Player> {
 
     @Override
     Player get();
